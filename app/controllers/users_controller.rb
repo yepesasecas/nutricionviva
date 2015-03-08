957cf3c4d7de592b34074251f335ab1cafb1daef
Andres Yepes <yepes07@gmail.com>
2015-03-07 20:41:41 -0500

    add users to DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      redirect_to root, notice: "Recibiras Consejos Saludables en tu email :)"
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
